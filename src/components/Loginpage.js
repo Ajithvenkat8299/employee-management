9b4639cdbdb4ac604ff46ff3141fb05c11a242b2
Ajithkumar V <67615212+Ajithvenkat8299@users.noreply.github.com>
2021-12-29 19:33:09 +0530

    Add files via upload

--- src/components/Loginpage.js
+++ src/components/Loginpage.js
@@ -1,91 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
 
-const appStyle = {
-	height: '250px',
-  	display: 'flex'
-};
+const Loginpage = () => {
+  return (
+    <div>
 
-const formStyle = {
-    margin: 'auto',
-    padding: '10px',
-    border: '1px solid #c9c9c9',
-    borderRadius: '5px',
-    background: '#f5f5f5',
-    width: '220px',
-  	display: 'block'
-};
 
-const labelStyle = {
-    margin: '10px 0 5px 0',
-    fontFamily: 'Arial, Helvetica, sans-serif',
-    fontSize: '15px',
-};
-
-const inputStyle = {
-    margin: '5px 0 10px 0',
-    padding: '5px', 
-    border: '1px solid #bfbfbf',
-    borderRadius: '3px',
-    boxSizing: 'border-box',
-    width: '100%'
-};
-
-const submitStyle = {
-    margin: '10px 0 0 0',
-    padding: '7px 10px',
-    border: '1px solid #efffff',
-    borderRadius: '3px',
-    background: '#3085d6',
-    width: '100%', 
-    fontSize: '15px',
-    color: 'white',
-    display: 'block'
-};
-
-const Field = React.forwardRef(({label, type}, ref) => {
-    return (
-      <div>
-        <label style={labelStyle} >{label}</label>
-        <input ref={ref} type={type} style={inputStyle} />
+      <div class="flex items-center justify-center min-h-screen bg-gray-100">
+          <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
+              <div class="flex justify-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-20 h-20 text-blue-600" fill="none" viewBox="0 0 24 24"
+                      stroke="currentColor">
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path
+                          d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                  </svg>
+              </div>
+              <h3 class="text-2xl font-bold text-center">Login to your account</h3>
+              <form action="">
+                  <div class="mt-4">
+                      <div>
+                          <label class="block" for="email">Email<label>
+                                  <input type="text" placeholder="Email"
+                                      class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+                                  <span class="text-xs tracking-wide text-red-600">Email field is required </span>
+                      </div>
+                      <div class="mt-4">
+                          <label class="block">Password<label>
+                                  <input type="password" placeholder="Password"
+                                      class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+                      </div>
+                      <div class="flex items-baseline justify-between">
+                          <button class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">Login</button>
+                          <a href="#" class="text-sm text-blue-600 hover:underline">Forgot password?</a>
+                      </div>
+                  </div>
+              </form>
+          </div>
       </div>
-    );
-});
 
-const Form = ({onSubmit}) => {
-    const usernameRef = React.useRef();
-    const passwordRef = React.useRef();
-    const handleSubmit = e => {
-        e.preventDefault();
-        const data = {
-            username: usernameRef.current.value,
-            password: passwordRef.current.value
-        };
-        onSubmit(data);
-    };
-    return (
-      <form style={formStyle} onSubmit={handleSubmit} >
-        <Field ref={usernameRef} label="Username:" type="text" />
-        <Field ref={passwordRef} label="Password:" type="password" />
-        <div>
-          <button style={submitStyle} type="submit">Submit</button>
-        </div>
-      </form>
-    );
-};
+    </div>
+  )
+}
 
-const App = () => {
-    const handleSubmit = data => {
-        const json = JSON.stringify(data, null, 4);
-        console.clear();
-        console.log(json);
-    };
-    return (
-      <div style={appStyle}>
-        <Form onSubmit={handleSubmit} />
-      </div>
-    );
-};
-
-const root = document.querySelector('#root');
-ReactDOM.render(<App />, root );
+export default Loginpage
