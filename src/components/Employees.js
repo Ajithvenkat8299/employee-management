f9efd238ca55b6bbc5a37c5147efe067e65f6f9f
Ajithkumar V <67615212+Ajithvenkat8299@users.noreply.github.com>
2021-12-30 19:22:51 +0530

    Add files via upload

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -3,45 +3,139 @@ import React from 'react'
 const Employees = () => {
     return (
         <div>
-            <table class="table-fixed">
-            <thead>
-                <tr>
-                <th class="w-1/2 px-4 py-2">Employee ID</th>
-                <th class="w-1/4 px-4 py-2">First name and Last name</th>
-                <th class="w-1/4 px-4 py-2">Email</th>
-                <th class="w-1/4 px-4 py-2">Department</th>
-                <th class="w-1/4 px-4 py-2">Description</th>
-                <th class="w-1/4 px-4 py-2">JoinedAt </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td class="border px-4 py-2">001</td>
-                <td class="border px-4 py-2">Jack roman</td>
-                <td class="border px-4 py-2">Jackroman@gmail.com</td>
-                <td class="border px-4 py-2">Movie</td>
-                <td class="border px-4 py-2"> -- </td>
-                <td class="border px-4 py-2">13/05/2021</td>
-                </tr>
-                <tr class="bg-gray-100">
-                <td class="border px-4 py-2">002</td>
-                <td class="border px-4 py-2">Adam jams</td>
-                <td class="border px-4 py-2">Adams@gmail.com</td>
-                <td class="border px-4 py-2">Production</td>
-                <td class="border px-4 py-2"> -- </td>
-                <td class="border px-4 py-2">06/11/2021</td>
-                </tr>
-                <tr>
-                <td class="border px-4 py-2">003</td>
-                <td class="border px-4 py-2">Chris morris</td>
-                <td class="border px-4 py-2">morris@gmail.com</td>
-                <td class="border px-4 py-2">Development</td>
-                <td class="border px-4 py-2"> -- </td>
-                <td class="border px-4 py-2">03/08/2021</td>
-                </tr>
-            </tbody>
-            </table>
+            <div>
+                <br></br>
+                   <div className = "container">
+                        <div className = "row">
+                            <div className = "card col-md-6 offset-md-3 offset-md-3">
+                                {
+                                    this.getTitle()
+                                }
+                                <div className = "card-body">
+                                    <form>
+                                        <div className = "form-group">
+                                            <label> First Name: </label>
+                                            <input placeholder="First Name" name="firstName" className="form-control" 
+                                                value={this.state.firstName} onChange={this.changeFirstNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Last Name: </label>
+                                            <input placeholder="Last Name" name="lastName" className="form-control" 
+                                                value={this.state.lastName} onChange={this.changeLastNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Email Id: </label>
+                                            <input placeholder="Email Address" name="emailId" className="form-control" 
+                                                value={this.state.emailId} onChange={this.changeEmailHandler}/>
+                                        </div>
+
+                                        <button className="btn btn-success" onClick={this.saveOrUpdateEmployee}>Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                   </div>
+            </div>
+            <div>
+                 <h2 className="text-center">Employees List</h2>
+                 <div className = "row">
+                    <button className="btn btn-primary" onClick={this.addEmployee}> Add Employee</button>
+                 </div>
+                 <br></br>
+                 <div className = "row">
+                        <table className = "table table-striped table-bordered">
+
+                            <thead>
+                                <tr>
+                                    <th> Employee First Name</th>
+                                    <th> Employee Last Name</th>
+                                    <th> Employee Email Id</th>
+                                    <th> Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.employees.map(
+                                        employee => 
+                                        <tr key = {employee.id}>
+                                             <td> { employee.firstName} </td>   
+                                             <td> {employee.lastName}</td>
+                                             <td> {employee.emailId}</td>
+                                             <td>
+                                                 <button onClick={ () => this.editEmployee(employee.id)} className="btn btn-info">Update </button>
+                                                 <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete </button>
+                                                 <button style={{marginLeft: "10px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-info">View </button>
+                                             </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+
+                 </div>
+
+            </div>
+
+            <div>
+                <br></br>
+                   <div className = "container">
+                        <div className = "row">
+                            <div className = "card col-md-6 offset-md-3 offset-md-3">
+                                <h3 className="text-center">Update Employee</h3>
+                                <div className = "card-body">
+                                    <form>
+                                        <div className = "form-group">
+                                            <label> First Name: </label>
+                                            <input placeholder="First Name" name="firstName" className="form-control" 
+                                                value={this.state.firstName} onChange={this.changeFirstNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Last Name: </label>
+                                            <input placeholder="Last Name" name="lastName" className="form-control" 
+                                                value={this.state.lastName} onChange={this.changeLastNameHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Email Id: </label>
+                                            <input placeholder="Email Address" name="emailId" className="form-control" 
+                                                value={this.state.emailId} onChange={this.changeEmailHandler}/>
+                                        </div>
+
+                                        <button className="btn btn-success" onClick={this.updateEmployee}>Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{marginLeft: "10px"}}>Cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                   </div>
+            </div>
+
+            <div>
+                <br></br>
+                <div className = "card col-md-6 offset-md-3">
+                    <h3 className = "text-center"> View Employee Details</h3>
+                    <div className = "card-body">
+                        <div className = "row">
+                            <label> Employee First Name: </label>
+                            <div> { this.state.employee.firstName }</div>
+                        </div>
+                        <div className = "row">
+                            <label> Employee Last Name: </label>
+                            <div> { this.state.employee.lastName }</div>
+                        </div>
+                        <div className = "row">
+                            <label> Employee Email ID: </label>
+                            <div> { this.state.employee.emailId }</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
+
+        
     )
 }
 
---
+++ src/components/SignUpFormValidation.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from "react";
+
+async function validate(refs, form) {
+  for (const [attribute, ref] of Object.entries(refs.current)) {
+    var errors;
+    if (ref.validate) {
+      errors = await ref.validate(get(form, attribute));
+    }
+    if (!isEmpty(errors)) {
+      console.log(errors);
+      return false;
+    }
+  }
+  return true;
+}
+
+export default function Signup(props) {
+  const { setAuthType } = props;
+  const [form, setForm] = useState({});
+  const [showPassword, setShowPassword] = useState();
+
+  const refs = useRef({});
+
+  const updateForm = (updates) => {
+    const copy = { ...form };
+    for (const [key, value] of Object.entries(updates)) {
+      set(copy, key, value);
+    }
+    setForm(copy);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const ok = await validate(refs, form);
+    if (!ok) {
+      return;
+    }
+    console.log(form);
+  };
+
+}
